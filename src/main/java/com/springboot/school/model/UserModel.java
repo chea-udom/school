39aec511b623c4e11e0fa3b8fcfe0ffe3e31ee0d
Chea Udom <chea_udom@ifinanceplc.com>
2022-09-01 15:42:13 +0700

    init project

---
+++ src/main/java/com/springboot/school/model/UserModel.java
@@ -0,0 +1,8 @@
+package com.springboot.school.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class UserModel extends PersonModel {
+    
+}
---
+++ src/main/java/com/springboot/school/view/MainView.java
@@ -0,0 +1,13 @@
+package com.springboot.school.views;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+
+@Route
+public class MainView extends VerticalLayout {
+    public MainView(){
+        add(new Text("Hello From Main"));
+    }
+}
